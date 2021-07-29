4f4f04e731c3c61fd9c5d8d7c2eb1f687727d3f1
Emil Gulayev <egulayev@ironscales.com>
2021-07-29 21:58:48 +0300

    init routing and game component

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,9 +1,32 @@
+import React,{useState,useEffect} from 'react'
+import {
+    Route,
+    Switch,
+    BrowserRouter,
+} from 'react-router-dom'
+
 import SignUp from './Components/SignUp/SignUp'
 
+function requireAuth(nextState, replace, next) {
+    if (!window.sessionStorage.getItem("token")) {
+        replace({
+            pathname: "/signup",
+            state: {nextPathname: nextState.location.pathname}
+        });
+    }
+    next();
+}
+
 function App() {
+    const [token,setToken] = useState("")
+
   return (
     <div className="App">
-      <SignUp></SignUp>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={SignUp} onEnter={requireAuth}></Route>
+            </Switch>
+        </BrowserRouter>
     </div>
   );
 }
