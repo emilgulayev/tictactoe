d3a5213c1b88c9d996d22b169a58d1d829eeaa6f
Emil Gulayev <egulayev@ironscales.com>
2021-07-30 09:37:20 +0300

    redirecting div

--- tic-tac-toe/src/Components/Game/Game.js
+++ tic-tac-toe/src/Components/Game/Game.js
@@ -1,26 +1,18 @@
 import React,{useState,useEffect} from 'react'
 import {Redirect} from 'react-router-dom'
-import ClipLoader from "react-spinners/ClipLoader"
 import { css } from "@emotion/react"
 import './Game.sass'
 
 function Game() {
 
     const [validToken,setValidtoken]=useState('')
-    const [color, setColor] = useState("#ffffff")
-
-    const override = css`
-      display: block;
-      margin: 0 auto;
-      border-color: red;
-    `
 
     useEffect(()=>{
         if(!window.sessionStorage.getItem("token")) setValidtoken(false)
         else setValidtoken(true)
     },[])
 
-    if(validToken==='') return <ClipLoader color={color} loading={true} css={override} size={100} />
+    if(validToken==='') return <div>Redirecting...</div>
     if(!validToken) return <Redirect to="/signup"></Redirect>
     return (
         <div className="Game">
